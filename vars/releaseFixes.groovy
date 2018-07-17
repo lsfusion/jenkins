a2e0ee6d31111d78f1a02bd087c7b8b71893ab76
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-17 14:11:54 +0300

    Test44

--- vars/releaseFixes.groovy
+++ vars/releaseFixes.groovy
@@ -2,6 +2,8 @@
 def call() {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
+        
+        dfdfdf
         pipeline {
                 agent any
                 stages {
