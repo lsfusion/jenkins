29bd760128310ddee334d5738d3fec9585f96249
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-17 12:50:07 +0300

    Test

--- vars/releaseFixes.groovy
+++ vars/releaseFixes.groovy
@@ -1,3 +1,6 @@
-def call() {
-    echo "Before run"
+// vars/releaseFixes.groovy
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
 }
