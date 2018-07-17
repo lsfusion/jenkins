ca81e16f7ef894a09371b65bc347812a5b5164b1
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-17 14:45:31 +0300

    Test44

--- vars/releaseFixes.groovy
+++ vars/releaseFixes.groovy
@@ -2,7 +2,7 @@ def call() {
         echo "Check status"
 
         (1..3).each {
-                sayHello "I am : " + it 
+                sayHello "I am am : " + it 
         }
 
 //        currentBuild.result = 'SUCCESS' //FAILURE to fail
