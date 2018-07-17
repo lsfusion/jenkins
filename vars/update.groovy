46d9c54dee62edfbc458c16fc8433ad82b528747
pavel-miniutka <asphalt.by@gmail.com>
2018-07-17 16:23:46 +0300

    deploySnapshots

--- vars/update.groovy
+++ vars/update.groovy
@@ -1,19 +1,21 @@
 def call(String jenkinsHome, String branch) {
-    stage('update') {
-        steps {
-            dir("${jenkinsHome}/src") {
-                dir('fsl') {
-                    git(
-                            url: 'https://github.com/lsfusion/fsl',
-                            branch: "${branch}"
-                    )
-                }
+    return {
+        stage('update') {
+            steps {
+                dir("${jenkinsHome}/src") {
+                    dir('fsl') {
+                        git(
+                                url: 'https://github.com/lsfusion/fsl',
+                                branch: "${branch}"
+                        )
+                    }
 
-                dir('platform') {
-                    git(
-                            url: 'https://github.com/lsfusion/platform',
-                            branch: "${branch}"
-                    )
+                    dir('platform') {
+                        git(
+                                url: 'https://github.com/lsfusion/platform',
+                                branch: "${branch}"
+                        )
+                    }
                 }
             }
         }
