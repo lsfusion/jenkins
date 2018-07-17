0924064dc20e96d0317ee8ec3f384df7deff5363
pavel-miniutka <asphalt.by@gmail.com>
2018-07-17 16:23:01 +0300

    deploySnapshots

--- vars/deploySnapshot.groovy
+++ vars/deploySnapshot.groovy
@@ -2,26 +2,29 @@ def call(String jenkinsHome, String branch) {
     def platform = "${jenkinsHome}/src/${branch}/platform"
     def fsl = "${jenkinsHome}/src/${branch}/fsl"
     def repos = '-DaltReleaseDeploymentRepository=lsfusion::default::http://repo.lsfusion.org/repository/releases -DaltSnapshotDeploymentRepository=lsfusion::default::http://repo.lsfusion.org/repository/snapshots'
-    
-    stages {
-        update "${jenkinsHome}", "${branch}"
-        
-        stage ('deploy') {
-            steps {
-                sh "mvn -f ${platform}/pom.xml clean"
-                sh "mvn -f ${platform}/base/pom.xml ${repos} deploy -P sources"
-                sh "mvn -f ${platform}/api/pom.xml ${repos} deploy -P sources"
-                sh "mvn -f ${platform}/report-api/pom.xml ${repos} deploy -P sources"
-                sh "mvn -f ${platform}/web-api/pom.xml ${repos} deploy -P sources"
-                sh "mvn -f ${platform}/logics/pom.xml ${repos} deploy -P sources"
 
-                sh "mvn -f ${fsl}/pom.xml clean"
-                sh "mvn -f ${fsl}/fsl-logics/pom.xml ${repos} deploy -P sources"
+    pipeline {
+        agent any
+        stages {
+            update "${jenkinsHome}", "${branch}"
 
-                sh "mvn -f ${platform}/server/pom.xml deploy -P sources,assemble"
-                sh "mvn -f ${fsl}/fsl-server/pom.xml deploy -P sources,assemble"
-                sh "mvn -f ${platform}/desktop-client/pom.xml deploy -P sources,assemble,sign,pack"
-                sh "mvn -f ${platform}/web-client/pom.xml deploy -P sources,gwt,desktop,war"
+            stage('deploy') {
+                steps {
+                    sh "mvn -f ${platform}/pom.xml clean"
+                    sh "mvn -f ${platform}/base/pom.xml ${repos} deploy -P sources"
+                    sh "mvn -f ${platform}/api/pom.xml ${repos} deploy -P sources"
+                    sh "mvn -f ${platform}/report-api/pom.xml ${repos} deploy -P sources"
+                    sh "mvn -f ${platform}/web-api/pom.xml ${repos} deploy -P sources"
+                    sh "mvn -f ${platform}/logics/pom.xml ${repos} deploy -P sources"
+
+                    sh "mvn -f ${fsl}/pom.xml clean"
+                    sh "mvn -f ${fsl}/fsl-logics/pom.xml ${repos} deploy -P sources"
+
+                    sh "mvn -f ${platform}/server/pom.xml deploy -P sources,assemble"
+                    sh "mvn -f ${fsl}/fsl-server/pom.xml deploy -P sources,assemble"
+                    sh "mvn -f ${platform}/desktop-client/pom.xml deploy -P sources,assemble,sign,pack"
+                    sh "mvn -f ${platform}/web-client/pom.xml deploy -P sources,gwt,desktop,war"
+                }
             }
         }
     }
