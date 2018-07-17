7368b5ac4555663d3280116a697d25fa2fdb023d
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-17 14:08:43 +0300

    Test44

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,5 +2,15 @@
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Hello, ${name}."
+    pipeline {
+        agent any
+        stages {
+            stage('Odd Stage') {
+                steps {
+                    echo "Hello, ${name}."
+                }
+            }
+        }
+    }
+
 }
