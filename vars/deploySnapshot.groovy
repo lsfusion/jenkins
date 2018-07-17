791d13d046f628778556e3f48e6e7ae71580c4b8
pavel-miniutka <asphalt.by@gmail.com>
2018-07-17 17:01:35 +0300

    deploySnapshots

--- vars/deploySnapshot.groovy
+++ vars/deploySnapshot.groovy
@@ -14,22 +14,39 @@ def call(String jenkinsHome, String branch) {
                 }
             }
 
-            stage('deploy') {
+            stage ('deploy jars') {
                 steps {
                     sh "mvn -f ${platform}/pom.xml clean"
+                    sh "mvn -f ${platform}/build/pom.xml ${repos} deploy -P sources"
                     sh "mvn -f ${platform}/base/pom.xml ${repos} deploy -P sources"
                     sh "mvn -f ${platform}/api/pom.xml ${repos} deploy -P sources"
+                    sh "mvn -f ${platform}/logics/pom.xml ${repos} deploy -P sources"
                     sh "mvn -f ${platform}/report-api/pom.xml ${repos} deploy -P sources"
                     sh "mvn -f ${platform}/web-api/pom.xml ${repos} deploy -P sources"
-                    sh "mvn -f ${platform}/logics/pom.xml ${repos} deploy -P sources"
 
                     sh "mvn -f ${fsl}/pom.xml clean"
                     sh "mvn -f ${fsl}/fsl-logics/pom.xml ${repos} deploy -P sources"
+                }
+            }
 
-                    sh "mvn -f ${platform}/server/pom.xml deploy -P sources,assemble"
-                    sh "mvn -f ${fsl}/fsl-server/pom.xml deploy -P sources,assemble"
-                    sh "mvn -f ${platform}/desktop-client/pom.xml deploy -P sources,assemble,sign,pack"
-                    sh "mvn -f ${platform}/web-client/pom.xml deploy -P sources,gwt,desktop,war"
+            stage('deploy') {
+                parallel {
+                    stage('server') {
+                        steps {
+                            sh "mvn -f ${platform}/server/pom.xml deploy -P sources,assemble"
+                        }
+                    }
+                    stage('client') {
+                        steps {
+                            sh "mvn -f ${platform}/desktop-client/pom.xml deploy -P sources,assemble,sign,pack"
+                            sh "mvn -f ${platform}/web-client/pom.xml deploy -P sources,gwt,desktop,war"
+                        }
+                    }
+                    stage('fsl-server') {
+                        steps {
+                            sh "mvn -f ${fsl}/fsl-server/pom.xml deploy -P sources,assemble"
+                        }
+                    }
                 }
             }
         }
