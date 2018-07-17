28f9b6f864bdbf2f0b0e654e427d109696faa28f
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-17 12:52:15 +0300

    Test

--- vars/releaseFixes.groovy
+++ vars/releaseFixes.groovy
@@ -2,5 +2,5 @@
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Helloddd, ${name}."
+    echo "Hello, ${name}."
 }
