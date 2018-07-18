f382dd801821f9758ccb1fa4a1140accee4c054c
pavel-miniutka <asphalt.by@gmail.com>
2018-07-18 14:44:03 +0300

    generate jnlp

--- vars/uploadToFtp.groovy
+++ vars/uploadToFtp.groovy
@@ -1,6 +1,6 @@
 def call(String filePath, String folder) {
     withCredentials([usernameColonPassword(credentialsId: 'lsfusion.ftp', variable: 'USERPASS')]) {
-        sh "curl -T ${filePath} --ftp-create-dirs ftp://ftp.keycdn.com/download/${folder}/ --user $USERPASS"
+        sh "curl -T ${filePath} --ftp-create-dirs ftp://ftp.keycdn.com/download/${folder}/ --user ${USERPASS}"
     }
 }
 
