d6c6c858a4bc10fa130c087d9e99df908f10dcf8
pavel-miniutka <asphalt.by@gmail.com>
2018-07-18 14:45:29 +0300

    JENKINS_HOME

--- src/Paths.groovy
+++ src/Paths.groovy
@@ -1,5 +1,4 @@
 class Paths {
-    static String jenkinsHome = '/usr/share/jenkins'
-    static String src = "${jenkinsHome}/src"
+    static String src = "${JENKINS_HOME}/src"
     static String tmp = "/tmp"
 }
