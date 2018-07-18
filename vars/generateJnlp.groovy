0b939c52d327279510ffa2f899f50666371f117e
pavel-miniutka <asphalt.by@gmail.com>
2018-07-18 12:00:45 +0300

    generate jnlp

--- vars/generateJnlp.groovy
+++ vars/generateJnlp.groovy
@@ -7,7 +7,7 @@ def call(String platformVersion) {
                         sh "cp -fa ${Paths.src}/platform/web-api/src/main/resources/client.jnlp ${Paths.tmp}/client-${platformVersion}.jnlp"
 
                         sh """sed -i -e 's|\${jnlp.codebase}|http://download.lsfusion.org|' \\
--e "s/\${jnlp.url}/client-${platformVersion}.jnlp/" \\
+-e "s/\$\"{jnlp.url}\"/client-${platformVersion}.jnlp/" \\
 -e 's|\${jnlp.appName}|lsFusion|' \\
 -e 's|\${jnlp.initHeapSize}|256m|' \\
 -e 's|\${jnlp.maxHeapSize}|1024m|' \\
