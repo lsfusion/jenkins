ef62018af12b3ebbd1943182eb3c8da21cba09e2
pavel-miniutka <asphalt.by@gmail.com>
2018-07-19 15:40:51 +0300

    update

--- vars/update.groovy
+++ vars/update.groovy
@@ -1,19 +1,29 @@
 def call(String branch) {
 //    stage('update') {
-        dir(Paths.src) {
-            dir('fsl') {
-                git(
-                        url: 'https://github.com/lsfusion/fsl',
-                        branch: branch
-                )
-            }
+    pipeline {
+        agent any
+        stages {
+            stage('update') {
+                steps {
+                    dir(Paths.src) {
+                        dir('fsl') {
+                            git(
+                                    url: 'https://github.com/lsfusion/fsl',
+                                    branch: branch
+                            )
+                        }
 
-            dir('platform') {
-                git(
-                        url: 'https://github.com/lsfusion/platform',
-                        branch: branch
-                )
+                        dir('platform') {
+                            git(
+                                    url: 'https://github.com/lsfusion/platform',
+                                    branch: branch
+                            )
+                        }
+                    }
+                }
+
             }
         }
+    }
 //    }
 }
