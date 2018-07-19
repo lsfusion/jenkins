47d418e855d08b9ced0844008985be17d2cc1723
pavel-miniutka <asphalt.by@gmail.com>
2018-07-19 15:08:37 +0300

    slack

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -5,3 +5,27 @@ def message(message) {
 def message(message, channel) {
     slackSend channel: channel, color: 'good', message: message
 }
+
+def warning(message) {
+    slackSend color: 'warning', message: message
+}
+
+def warning(message, channel) {
+    slackSend channel: channel, color: 'warning', message: message
+}
+
+def error(message) {
+    slackSend color: 'danger', message: message
+}
+
+def error(message, channel) {
+    slackSend channel: channel, color: 'danger', message: message
+}
+
+def send(message, color) {
+    slackSend color: 'danger', message: message
+}
+
+def send(message, color, channel) {
+    slackSend channel: channel, color: color, message: message
+}
