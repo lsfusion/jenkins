0ad333e83fe8c6f62b8ca23d61aaf47f0aa94cc0
pavel-miniutka <asphalt.by@gmail.com>
2018-07-20 17:56:10 +0300

    deploy rc

--- vars/deploySnapshot.groovy
+++ vars/deploySnapshot.groovy
@@ -1,15 +1,13 @@
 def call(String branch) {
-    return {
-        stage('update and deploy') {
-            stages {
-                script {
-                    update branch
-                }
+    stage('update and deploy') {
+        stages {
+            script {
+                update branch
+            }
 
-                stage('deploy') {
-                    steps {
-                        sh "mvn -f ${Paths.src}/pom.xml clean deploy"
-                    }
+            stage('deploy') {
+                steps {
+                    sh "mvn -f ${Paths.src}/pom.xml clean deploy"
                 }
             }
         }
