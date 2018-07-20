d55f7c86f7dfb105f52a109d19064646e83295b7
pavel-miniutka <asphalt.by@gmail.com>
2018-07-20 14:36:18 +0300

    removed fsl

--- vars/deployRC.groovy
+++ vars/deployRC.groovy
@@ -7,21 +7,12 @@ def call() {
 
     node {
         stage('pull') {
-            dir("${Paths.jenkinsHome}/rc") {
-                dir('platform-RC') {
-                    git(
-                            poll: true,
-                            url: 'https://github.com/lsfusion/platform',
-                            branch: 'release-candidate'
-                    )
-                }
-                dir('fsl-RC') {
-                    git(
-                            poll: true,
-                            url: 'https://github.com/lsfusion/fsl',
-                            branch: 'release-candidate'
-                    )
-                }
+            dir("${Paths.jenkinsHome}/rc/platform-RC") {
+                git(
+                        poll: true,
+                        url: 'https://github.com/lsfusion/platform',
+                        branch: 'release-candidate'
+                )
             }
         }
     }
@@ -33,9 +24,7 @@ def call() {
                 agent any
                 steps {
                     build 'saveLatestRCCommitToFile'
-                    build 'cleanRC'
-                    sh "mvn -f ${Paths.jenkinsHome}/rc/platform-RC/pom.xml deploy"
-                    sh "mvn -f ${Paths.jenkinsHome}/rc/fsl-RC/pom.xml deploy"
+                    sh "mvn -f ${Paths.jenkinsHome}/rc/platform-RC/pom.xml clean deploy"
                 }
             }
         }
@@ -47,7 +36,7 @@ def call() {
                         message: "<${env.BUILD_URL}|${currentBuild.fullDisplayName}> успешно завершён.\n```" + getCommitMessage() + "```"
             }
             failure {
-                slackSend channel: '#dev',
+                slackSend channel: '#dev',                   
                         color: 'danger',
                         message: "Внимание! <${env.BUILD_URL}|${currentBuild.fullDisplayName}> завершился с ошибкой.\n```" + getCommitMessage() + "```"
             }
@@ -59,15 +48,5 @@ def call() {
 def getCommitMessage() {
     def logPlatform = new XmlSlurper().parse("${Paths.jenkinsHome}/rc/platformLatestCommit")
     assert logPlatform instanceof groovy.util.slurpersupport.GPathResult
-    def datePlatform = Date.parse('yyyy-MM-dd HH:mm:ss ZZ', logPlatform.date.text())
-
-    def logFsl = new XmlSlurper().parse("${Paths.jenkinsHome}/rc/fslLatestCommit")
-    assert logFsl instanceof groovy.util.slurpersupport.GPathResult
-    def dateFsl = Date.parse('yyyy-MM-dd HH:mm:ss ZZ', logFsl.date.text())
-
-    if (datePlatform >= dateFsl)  {
-        return logPlatform.msg.text() + '\nby ' + logPlatform.author.text()
-    } else {
-        return logFsl.msg.text() + '\nby ' + logFsl.author.text()
-    }
+    return logPlatform.msg.text() + '\nby ' + logPlatform.author.text()
 }
--- vars/update.groovy
+++ vars/update.groovy
@@ -1,19 +1,10 @@
 def call(String branch) {
     stage('update') {
-        dir(Paths.src) {
-            dir('fsl') {
-                git(
-                        url: 'https://github.com/lsfusion/fsl',
-                        branch: branch
-                )
-            }
-
-            dir('platform') {
-                git(
-                        url: 'https://github.com/lsfusion/platform',
-                        branch: branch
-                )
-            }
+        dir("${Paths.src}/platfom") {
+            git(
+                    url: 'https://github.com/lsfusion/platform',
+                    branch: branch
+            )
         }
     }
 }
