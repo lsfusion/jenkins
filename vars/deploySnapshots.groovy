b30c486d1359aea37396a53120452bfbd88c1ac4
pavel-miniutka <asphalt.by@gmail.com>
2018-07-23 16:15:20 +0300

    deploy snapshots

--- vars/deploySnapshots.groovy
+++ vars/deploySnapshots.groovy
@@ -1,7 +1,8 @@
-def call(body) {
-    def branches = ['release-candidate']
-    for (branch in branches) {
-        deploySnapshot branch
-    }
+def call() {
+    echo 'deploy'
+//    def branches = ['release-candidate']
+//    for (branch in branches) {
+//        deploySnapshot branch
+//    }
 }
 
