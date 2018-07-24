cd422478b8039204824e75640343a62f725c849f
pavel-miniutka <asphalt.by@gmail.com>
2018-07-24 12:46:33 +0300

    update ERP git mirror

--- vars/deploySnapshot.groovy
+++ vars/deploySnapshot.groovy
@@ -1,6 +1,6 @@
 def call(String branch) {
-    echo 'update'
-    update branch
-    echo 'deploy'
-    println "mvn -f ${Paths.src}/pom.xml clean deploy".execute().text
+    node {
+        update branch
+        sh "mvn -f ${Paths.src}/pom.xml clean deploy"
+    }
 }
