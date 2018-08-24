d2bfe7c3e86eef9a63632a8b8c3257b234548b94
pavel-miniutka <asphalt.by@gmail.com>
2018-08-24 14:32:00 +0300

    buildAndUploadPlugin task

---
+++ vars/buildAndUploadPlugin.groovy
@@ -0,0 +1,43 @@
+call() {
+    try {
+        git(
+                url: 'https://github.com/lsfusion/plugin-idea',
+                branch: 'master',
+        )
+
+        def versionFile = "${Paths.jenkinsHome}/idea-plugin/currentPluginVersion"
+        def oldVer = readFile versionFile
+        def newVer = getPluginVersion()
+        if (newVer != oldVer) {
+            writeFile file: versionFile, text: newVer
+            sh 'ant all'
+
+            sh "cp -f lsfusion-idea-plugin.zip ${Paths.jenkinsHome}/installer-src/"
+
+            sh 'sh upload-plugin.sh'
+
+            slack.message "Plugin v.${getPluginVersion()} was built successfully.\n```${getReleaseNotes()}```"
+        } else {
+            echo "version's the same"
+        }
+    } catch (e) {
+        slack.error "Warning! <$env.BUILD_URL|$currentBuild.fullDisplayName> failed."
+        throw e
+    }
+}
+
+@NonCPS
+def getPluginVersion() {
+    return new XmlSlurper().parse("${Paths.jenkinsHome}/idea-plugin/META-INF/plugin.xml").version.text()
+}
+
+@NonCPS
+def getReleaseNotes() {
+    def text = new File("${Paths.jenkinsHome}/idea-plugin/META-INF/plugin.xml").text
+    def lis = new XmlSlurper().parseText(text.substring(text.indexOf("<ul>", 0), text.indexOf("</ul>", 0) + 5))
+    def res = ""
+    for (li in lis.children()) {
+        res = "${res}• ${li.text()}\n"
+    }
+    return res
+}
