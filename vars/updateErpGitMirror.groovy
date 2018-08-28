0ca1125d66b6d7cb7d522703e41ade035d46c84c
pavel-miniutka <asphalt.by@gmail.com>
2018-08-28 10:16:52 +0300

    updateErpGitMirror

--- vars/updateErpGitMirror.groovy
+++ vars/updateErpGitMirror.groovy
@@ -1,17 +1,9 @@
 def call() {
-    pipeline {
-        agent any
-        stages {
-            stage('update') {
-                steps {
-                    dir("${Paths.jenkinsHome}/erp-git-mirror") {
-                        sh "git svn fetch"
-                        sh "git rebase remotes/trunk"
-                        sh "git push origin master"
-                    }
-                }
-            }
-        }
+    withCredentials([usernamePassword(credentialsId: '48021276-4f5f-4293-975e-ee13d9f97f51', passwordVariable: 'SVN_PASSWORD', usernameVariable: 'SVN_USERNAME')]) {
+        sh "printf ${SVN_PASSWORD} | git svn fetch --username ${SVN_USERNAME}"
     }
+    
+    sh "git rebase remotes/trunk"
+    sh "git push origin master"
 }
 
