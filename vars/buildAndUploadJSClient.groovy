e77fa1be3aa0bc173fe9ed16d2ecae014a9827b6
pavel-miniutka <asphalt.by@gmail.com>
2019-04-15 16:21:03 +0300

    buildAndUploadJSClient

---
+++ vars/buildAndUploadJSClient.groovy
@@ -0,0 +1,30 @@
+import groovy.json.JsonSlurper
+
+def call() {
+    try {
+        git(
+                url: 'https://github.com/lsfusion/js-client',
+                branch: 'master',
+        )
+
+        def versionFile = "currentJSClientVersion"
+        def oldVer = readFile versionFile
+        def newVer = new JsonSlurper().parseText(new File("${Paths.jenkinsHome}/js-client/core/package.json").text).version
+        if (newVer != oldVer) {
+            echo "new version ${newVer}"
+
+            // dir("core") {
+            //     sh 'npm install --access public'
+            // }
+
+            // slack.message "JS Client v.${newVer} was built successfully."
+
+            writeFile file: versionFile, text: newVer
+        } else {
+            echo "version's the same"
+        }
+    } catch (e) {
+        // slack.error "Warning! <$env.BUILD_URL|$currentBuild.fullDisplayName> failed."
+        throw e
+    }
+}
