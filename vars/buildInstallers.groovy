91a011d66b8583799a9b87ea10a958dbc6f09e15
AlexKirkouski <alex.kirkouski@gmail.com>
2019-06-13 16:56:05 +0300

    Removed SNAPSHOT from VI version

--- vars/buildInstallers.groovy
+++ vars/buildInstallers.groovy
@@ -27,7 +27,7 @@ def call(int majorVersion, String platformVersion) {
 
         sh "echo '\n!define LSFUSION_MAJOR_VERSION ${majorVersion}' >> Versions.nsh"
         sh "echo '\n!define LSFUSION_VERSION ${platformVersion}' >> Versions.nsh"
-        String viVersion = platformVersion.replace('beta', '999') + '.0'
+        String viVersion = platformVersion.replace('beta', '999').replace('-SNAPSHOT', '') + '.0'
         if (!platformVersion.contains('beta')) {
             viVersion += '.0'
         }
