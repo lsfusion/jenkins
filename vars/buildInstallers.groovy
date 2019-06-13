d5037385ab97aa495f048e989add418ee7d3aa09
pavel-miniutka <asphalt.by@gmail.com>
2019-06-13 15:34:00 +0300

    fixed installer folder

--- vars/buildInstallers.groovy
+++ vars/buildInstallers.groovy
@@ -23,7 +23,7 @@ def call(int majorVersion, String platformVersion) {
 
     dir(workspace) {
         def makensis = "${installerSrc}/nsis-unicode-win/makensis.exe"
-        def downloadDir = "${Paths.download}/exe/${platformVersion}"
+        def downloadDir = "${Paths.download}/exe"
 
         sh "echo '\n!define LSFUSION_MAJOR_VERSION ${majorVersion}' >> Versions.nsh"
         sh "echo '\n!define LSFUSION_VERSION ${platformVersion}' >> Versions.nsh"
