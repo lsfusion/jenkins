162700c10341c084288e9f499630c6f043df0dbb
pavel-miniutka <asphalt.by@gmail.com>
2019-06-17 17:30:20 +0300

    build RPM installers

---
+++ resources/installer/yum/server/lsfusion.spec
@@ -0,0 +1,85 @@
+%define __jar_repack %{nil}
+%define lsfusion_major_version <lsfusion-major-version>
+%define lsfusion_version <lsfusion-version>
+%define lsfusion_title <lsfusion-title>
+%define lsfusion_name lsfusion%{lsfusion_major_version}-server
+%define lsfusion_home /usr/share/%{lsfusion_name}
+%define lsfusion_group lsfusion
+%define lsfusion_user lsfusion
+
+Summary:    %{lsfusion_title}
+Name:       %{lsfusion_name}
+Version:    %{lsfusion_version}
+BuildArch:  noarch
+Release:    1
+License:    GNU Lesser General Public License v3.0
+Group:      Networking/Daemons
+URL:        https://lsfusion.org
+Source0:    server.jar
+Source1:    lsfusion.conf
+Source2:    settings.properties
+Source3:    %{lsfusion_name}.service
+Requires:   java-1.8.0-headless
+Requires:   jsvc
+Conflicts:  tomcat, tomcat7, tomcat8
+BuildRoot:  %{_tmppath}/lsfusion%{lsfusion_major_version}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+%description
+%{lsfusion_title}
+
+%prep
+
+%build
+
+%install
+install -d -m 755 %{buildroot}/%{lsfusion_home}/
+install    -m 644 %{SOURCE0} %{buildroot}/%{lsfusion_home}
+
+# Put logging in /var/log and link back.
+install -d -m 755 %{buildroot}/var/log/%{lsfusion_name}/
+cd %{buildroot}/%{lsfusion_home}/
+ln -s /var/log/%{lsfusion_name}/ logs
+cd -
+
+# Put conf in /etc/ and link back.
+install -d -m 755 %{buildroot}/%{_sysconfdir}/%{lsfusion_name}
+install    -m 644 %{SOURCE2} %{buildroot}/%{_sysconfdir}/%{lsfusion_name}
+install    -m 644 %{SOURCE1} %{buildroot}/%{_sysconfdir}/%{lsfusion_name}
+cd %{buildroot}/%{lsfusion_home}/
+ln -s %{_sysconfdir}/%{lsfusion_name} conf
+cd -
+
+# Drop init script
+install -d -m 755 %{buildroot}/%{_unitdir}
+install    -m 644 %{SOURCE3} %{buildroot}/%{_unitdir}
+
+# Create lib directory
+install -d -m 755 %{buildroot}/%{_sharedstatedir}/lsfusion
+
+# Create run directory
+install -d -m 755 %{buildroot}/%{_rundir}/%{lsfusion_name}
+
+%clean
+rm -rf %{buildroot}
+
+%pre
+getent group %{lsfusion_group} >/dev/null || groupadd %{lsfusion_group}
+getent passwd %{lsfusion_user} >/dev/null || useradd --comment "lsFusion Daemon User" -M -s /bin/nologin -g %{lsfusion_group} %{lsfusion_user}
+
+%files
+%defattr(-,root,root)
+%{_unitdir}/%{lsfusion_name}.service
+%{lsfusion_home}
+%{_sharedstatedir}/lsfusion
+%config(noreplace) %{_sysconfdir}/%{lsfusion_name}
+%defattr(-,%{lsfusion_user},%{lsfusion_group})
+%{_rundir}/%{lsfusion_name}
+/var/log/%{lsfusion_name}
+
+%post
+
+%preun
+
+%postun
+
+%changelog
---
+++ vars/buildRPMInstallers.groovy
@@ -0,0 +1,101 @@
+def rpmDir = "${Paths.jenkinsHome}/rpm"
+def yumDir = "$rpmDir/yum"
+
+def call(int majorVersion, String platformVersion) {
+    def downloadDir = "${Paths.download}/yum/${platformVersion}"
+    
+    buildServerInstaller(majorVersion, platformVersion)
+    buildClientInstaller(majorVersion, platformVersion)
+
+    dir(rpmDir) {
+        sh 'createrepo --update yum'
+
+        sh "mkdir -p ${downloadDir}"
+        sh "cp -f yum/* ${downloadDir}/"
+
+        ftpPublisher failOnError: true, publishers: [
+                [configName: 'Download FTP server',
+                 transfers : [
+                         [sourceFiles: "${downloadDir}/", remoteDirectory: "yum", removePrefix: "${downloadDir}"]
+//                         [sourceFiles: "yum/"]
+                 ],
+                 verbose   : true]
+        ]
+    }
+}
+
+def buildServerInstaller(int majorVersion, String platformVersion) {
+    def title = "lsFusion $majorVersion Server"
+    def serverName = "lsfusion$majorVersion-server"
+    def templatesDir = getResourcesDir() + '/installer/yum/server'
+
+    dir(rpmDir) {
+        sh 'rm -rf rpmbuild'
+        sh 'mkdir rpmbuild'
+
+        dir ('rpmbuild') {
+            sh 'mkdir BUILD'
+            sh 'mkdir RPMS'
+            sh 'mkdir SOURCES'
+            sh 'mkdir SPECS'
+            sh 'mkdir SRPMS'
+            
+            sh "sed 's/<lsfusion-server>/$serverName/g' $templatesDir/lsfusion.conf > SOURCES/lsfusion.conf"
+            sh "sed 's/<lsfusion-server>/$serverName/g; s/<lsfusion-description>/$title/g' $templatesDir/lsfusion-server.service > SOURCES/${serverName}.service"
+            sh "sed 's/<lsfusion-major-version>/$majorVersion/g; s/<lsfusion-version>/$platformVersion/g; s/<lsfusion-title>/$title/g' $templatesDir/lsfusion.spec > SPECS/lsfusion.spec"
+            
+            sh "cp -fa $templatesDir/settings.properties SOURCES/"
+            
+            sh "mvn -f ${Paths.src}/pom.xml dependency:copy -Dartifact=lsfusion.platform:server:$platformVersion:jar:assembly -DoutputDirectory=SOURCES/"
+            sh "mv -f SOURCES/server-$platformVersion-assembly.jar SOURCES/server.jar"
+
+            sh '''#!/usr/bin/expect
+set timeout -1
+spawn bash -c {rpmbuild --buildroot `pwd`/BUILDROOT --sign SPECS/lsfusion.spec -bb --define \"_topdir `pwd`\"}
+expect -exact "Enter pass phrase: "
+send -- "f7y45cnb\r"
+expect eof'''
+            
+            sh "cp -fa RPMS/noarch/* $yumDir/"
+        }
+    }
+}
+
+def buildClientInstaller(int majorVersion, String platformVersion) {
+    def title = "lsFusion $majorVersion Client"
+    def clientName = "lsfusion$majorVersion-client"
+    def templatesDir = getResourcesDir() + '/installer/yum/client'
+
+    dir(rpmDir) {
+        sh 'rm -rf rpmbuild'
+        sh 'mkdir rpmbuild'
+
+        dir ('rpmbuild') {
+            sh 'mkdir BUILD'
+            sh 'mkdir RPMS'
+            sh 'mkdir SOURCES'
+            sh 'mkdir SPECS'
+            sh 'mkdir SRPMS'
+
+            sh "sed 's/<lsfusion-client>/$clientName/g' $templatesDir/lsfusion.conf > SOURCES/lsfusion.conf"
+            sh "sed 's/<lsfusion-client>/$clientName/g' $templatesDir/lsfusion.logrotate > SOURCES/lsfusion.logrotate"
+            sh "sed 's/<lsfusion-client>/$clientName/g; s/<lsfusion-description>/$title/g' $templatesDir/lsfusion-client.service > SOURCES/${clientName}.service"
+            sh "sed 's/<lsfusion-major-version>/$majorVersion/g; s/<lsfusion-version>/$platformVersion/g; s/<lsfusion-title>/$title/g' $templatesDir/lsfusion.spec > SPECS/lsfusion.spec"
+
+            sh 'cp -fa ../apache-tomcat-9.0.20.tar.gz SOURCES/'
+            sh "cp -fa $templatesDir/ROOT.xml SOURCES/"
+
+            sh "mvn -f ${Paths.src}/pom.xml dependency:copy -Dartifact=lsfusion.platform:web-client:$platformVersion:war -DoutputDirectory=SOURCES/"
+            sh "mv -f SOURCES/web-client-${platformVersion}.war SOURCES/client.war"
+
+            sh '''#!/usr/bin/expect
+set timeout -1
+spawn bash -c {rpmbuild --buildroot `pwd`/BUILDROOT --sign SPECS/lsfusion.spec -bb --define \"_topdir `pwd`\"}
+expect -exact "Enter pass phrase: "
+send -- "f7y45cnb\r"
+expect eof'''
+            
+            sh "cp -fa RPMS/noarch/* $yumDir/"
+        }
+    }
+}
