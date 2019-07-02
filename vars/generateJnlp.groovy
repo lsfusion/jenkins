2088d33fe451e6d49fc91d54a9d1182d6ccc3d01
pavel-miniutka <asphalt.by@gmail.com>
2019-07-02 10:10:11 +0300

    generate JNLP fix

--- vars/generateJnlp.groovy
+++ vars/generateJnlp.groovy
@@ -4,7 +4,7 @@ def call(String platformVersion) {
     
     sh "cp -fa server/src/main/resources/client.jnlp ${downloadDir}/lsfusion-client-${platformVersion}.jnlp"
 
-    sh """sed -i -e 's|\${jnlp.codebase}|http://download.lsfusion.org/java|' \\
+    sh """sed -i -e 's|\${jnlp.codebase}|https://download.lsfusion.org/java|' \\
 -e "s/\$\"{jnlp.url}\"/lsfusion-client-${platformVersion}.jnlp/" \\
 -e 's|\${jnlp.appName}|lsFusion|' \\
 -e 's|\${jnlp.initHeapSize}|32m|' \\
