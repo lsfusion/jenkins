58d2a97ce35635801bf19de2af6b98fe2e165204
pavel-miniutka <asphalt.by@gmail.com>
2020-08-21 15:30:18 +0300

    release fixes

--- vars/releaseFixes.groovy
+++ vars/releaseFixes.groovy
@@ -1,5 +1,12 @@
-
 // when there are enough bugfixes / every 2-4 weeks (except beta / final releases) - for using in production (later versions are more stable)
-def call() {
-        releaseVersions false, false
+def call(int version) {
+    int minorVersion
+    boolean isBeta
+    (isBeta, minorVersion) = getBranchVersion(version)
+
+    if (isBeta) {
+        error("Version should be FINAL")
+    }
+
+    releaseVersion version, false
 }
