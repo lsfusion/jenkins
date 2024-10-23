2ee72d2d9a31a050f5c5a8e1f19fa532cec5d495
pavel-miniutka <asphalt.by@gmail.com>
2024-10-23 12:25:56 +0300

    snapshot installers test: rpm

--- vars/buildSnapshotInstallers.groovy
+++ vars/buildSnapshotInstallers.groovy
@@ -4,19 +4,20 @@ def call(String branch) {
     String platformVersion = readVersion()
     int majorVersion = platformVersion.replaceFirst(/\.[0-9]+-SNAPSHOT/, '') as Integer
 
-    buildWindowsInstallers(majorVersion, platformVersion)
+//    buildWindowsInstallers(majorVersion, platformVersion)
+    buildRPMInstallers(majorVersion, platformVersion)
 
     dir(Paths.download) {
-        ftpPublisher failOnError: true, publishers: [
-                [configName: 'Download FTP server',
-                 transfers : [
-                         [sourceFiles: "exe/${platformVersion}/", remoteDirectory: "exe", flatten: true],
-//                                 [sourceFiles: "apt/", remoteDirectory: "apt", removePrefix: "apt"],
-//                                 [sourceFiles: "dnf/", remoteDirectory: "dnf", removePrefix: "dnf"],
-//                                 [sourceFiles: "docker/${tagVersion}/", remoteDirectory: "docker", removePrefix: "docker"]
-                 ],
-                 verbose   : true]
-        ]
+//        ftpPublisher failOnError: true, publishers: [
+//                [configName: 'Download FTP server',
+//                 transfers : [
+//                         [sourceFiles: "exe/${platformVersion}/", remoteDirectory: "exe", flatten: true],
+////                                 [sourceFiles: "apt/", remoteDirectory: "apt", removePrefix: "apt"],
+////                                 [sourceFiles: "dnf/", remoteDirectory: "dnf", removePrefix: "dnf"],
+////                                 [sourceFiles: "docker/${tagVersion}/", remoteDirectory: "docker", removePrefix: "docker"]
+//                 ],
+//                 verbose   : true]
+//        ]
     }
 }
 
