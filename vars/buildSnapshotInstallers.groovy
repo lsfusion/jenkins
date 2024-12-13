17d1b82255fa66975a5841db2fd206a1579a4870
pavel-miniutka <asphalt.by@gmail.com>
2024-12-13 15:16:15 +0300

    snapshot installers test: apt

--- vars/buildSnapshotInstallers.groovy
+++ vars/buildSnapshotInstallers.groovy
@@ -8,17 +8,17 @@ def call(String branch) {
 //    buildRPMInstallers(majorVersion, platformVersion)
     buildAPTInstallers(majorVersion, platformVersion)
 
-    dir(Paths.download) {
-        ftpPublisher failOnError: true, publishers: [
-                [configName: 'Download FTP server',
-                 transfers : [
-//                         [sourceFiles: "exe/${platformVersion}/", remoteDirectory: "exe", flatten: true],
-                                 [sourceFiles: "apt-snap/", remoteDirectory: "apt-snap", removePrefix: "apt-snap", cleanRemote: true],
-//                                 [sourceFiles: "dnf-snap/", remoteDirectory: "dnf-snap", removePrefix: "dnf-snap", cleanRemote: true],
-//                                 [sourceFiles: "docker/${tagVersion}/", remoteDirectory: "docker", removePrefix: "docker"]
-                 ],
-                 verbose   : true]
-        ]
-    }
+//    dir(Paths.download) {
+//        ftpPublisher failOnError: true, publishers: [
+//                [configName: 'Download FTP server',
+//                 transfers : [
+////                         [sourceFiles: "exe/${platformVersion}/", remoteDirectory: "exe", flatten: true],
+//                                 [sourceFiles: "apt-snap/", remoteDirectory: "apt-snap", removePrefix: "apt-snap", cleanRemote: true],
+////                                 [sourceFiles: "dnf-snap/", remoteDirectory: "dnf-snap", removePrefix: "dnf-snap", cleanRemote: true],
+////                                 [sourceFiles: "docker/${tagVersion}/", remoteDirectory: "docker", removePrefix: "docker"]
+//                 ],
+//                 verbose   : true]
+//        ]
+//    }
 }
 
