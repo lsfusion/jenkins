8210b4e3305f4015d6e6d266345be34cd1e5ad8e
pavel-miniutka <asphalt.by@gmail.com>
2024-12-20 14:41:47 +0300

    snapshot installers test: apt

--- vars/buildAPTInstallers.groovy
+++ vars/buildAPTInstallers.groovy
@@ -10,7 +10,7 @@ def call(int majorVersion, String platformVersion) {
         currentAptRelease = readLatestSnapshotRelease(platformVersion)
     }
     
-    aptVersion = platformVersion + '.' + currentAptRelease + '-1'
+    aptVersion = platformVersion + '-' + currentAptRelease
     
     buildServerInstaller(majorVersion, platformVersion, aptVersion)
     buildClientInstaller(majorVersion, platformVersion, aptVersion)
