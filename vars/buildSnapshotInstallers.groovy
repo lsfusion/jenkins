9dd2f7a77bc873e471adf3801e7dfc46df51c12b
pavel-miniutka <pavel.miniutka@gmail.com>
2025-03-03 11:21:15 +0300

    Build all

--- vars/buildSnapshotInstallers.groovy
+++ vars/buildSnapshotInstallers.groovy
@@ -4,9 +4,9 @@ def call(String branch) {
     String platformVersion = readVersion()
     int majorVersion = platformVersion.replaceFirst(/\.[0-9]+-SNAPSHOT/, '') as Integer
 
-//    buildWindowsInstallers(majorVersion, platformVersion)
-//    buildRPMInstallers(majorVersion, platformVersion)
-//    buildAPTInstallers(majorVersion, platformVersion)
+    buildWindowsInstallers(majorVersion, platformVersion)
+    buildRPMInstallers(majorVersion, platformVersion)
+    buildAPTInstallers(majorVersion, platformVersion)
 
     buildAndDeployDockerImages(platformVersion)
     sh "mkdir -p ${Paths.download}/docker/${platformVersion}"
@@ -16,10 +16,10 @@ def call(String branch) {
         ftpPublisher failOnError: true, publishers: [
                 [configName: 'Download FTP server',
                  transfers : [
-//                         [sourceFiles: "exe/${platformVersion}/", remoteDirectory: "exe", flatten: true],
-                                 [sourceFiles: "apt-snap/", remoteDirectory: "apt-snap", removePrefix: "apt-snap", cleanRemote: true],
-//                                 [sourceFiles: "dnf-snap/", remoteDirectory: "dnf-snap", removePrefix: "dnf-snap", cleanRemote: true],
-//                                 [sourceFiles: "docker/${tagVersion}/", remoteDirectory: "docker", removePrefix: "docker"]
+                         [sourceFiles: "exe/${platformVersion}/", remoteDirectory: "exe", flatten: true],
+                         [sourceFiles: "apt-snap/", remoteDirectory: "apt-snap", removePrefix: "apt-snap", cleanRemote: true],
+                         [sourceFiles: "dnf-snap/", remoteDirectory: "dnf-snap", removePrefix: "dnf-snap", cleanRemote: true],
+                         [sourceFiles: "docker/${tagVersion}/", remoteDirectory: "docker", removePrefix: "docker"]
                  ],
                  verbose   : true]
         ]
