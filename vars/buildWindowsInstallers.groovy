8279829506a72e03d0729500ca14d566d25c0fb9
pavel-miniutka <pavel.miniutka@gmail.com>
2025-04-21 15:42:13 +0300

    Windows installer fix

--- vars/buildWindowsInstallers.groovy
+++ vars/buildWindowsInstallers.groovy
@@ -30,7 +30,7 @@ def call(int majorVersion, String platformVersion) {
         sh "echo '\n!define LSFUSION_VERSION ${platformVersion.replace('-SNAPSHOT', '')}' >> Versions.nsh"
         sh "echo '\n!define LSFUSION_LIBRARIES_VERSION ${platformVersion.replaceFirst(/\.[0-9]+-SNAPSHOT/, '-SNAPSHOT')}' >> Versions.nsh"
         String viVersion = platformVersion.replace('-beta', '.999.').replace('-SNAPSHOT', '.111.0')
-        if (!platformVersion.contains('beta') && !platformVersion.concat('-SNAPSHOT')) {
+        if (!platformVersion.contains('beta') && !platformVersion.contains('-SNAPSHOT')) {
             viVersion += '.0.0'
         }
         sh "echo '\n!define VI_LSFUSION_VERSION ${viVersion}' >> Versions.nsh"
