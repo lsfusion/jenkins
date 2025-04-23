c6ad98e60523679a953bdc517ca367fb953c5abc
pavel-miniutka <pavel.miniutka@gmail.com>
2025-04-23 11:54:05 +0300

    Revert "finish release"
    This reverts commit 0e17cc40e42eb7f462e0206c888bdc9c21772e86.

--- vars/releaseFinalFeature.groovy
+++ vars/releaseFinalFeature.groovy
@@ -3,20 +3,20 @@ Integer lastVersion, lastSupportedVersion
 
 // when there are no new bug reports / in 2 months after beta - public final release, for using in production
 def call() {
-//    stage('Read last version') {
-//        (lastVersion, lastVersionState, lastSupportedVersion) = getLastVersions()
-//    }
-//
-//    stage('Check last version state') {
-//        if (!lastVersionState.equals("BETA"))
-//            error("Last version should be BETA")
-//    }
-//    stage('Update') {
-//        update "v$lastVersion"
-//    }
+    stage('Read last version') {
+        (lastVersion, lastVersionState, lastSupportedVersion) = getLastVersions()
+    }
+
+    stage('Check last version state') {
+        if (!lastVersionState.equals("BETA"))
+            error("Last version should be BETA")
+    }
+    stage('Update') {
+        update "v$lastVersion"
+    }
 
     stage('Release final') {
-        releaseVersion 6, true
+        releaseVersion lastVersion, true
     }
 }
 
