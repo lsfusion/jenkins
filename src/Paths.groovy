e059538209efe455278f29121f17367ef442236a
pavel-miniutka <pavel.miniutka@gmail.com>
2026-01-06 10:29:04 +0300

    Changed some paths

--- src/Paths.groovy
+++ src/Paths.groovy
@@ -1,19 +1,19 @@
 class Paths {
     static String jenkinsHome = '/usr/share/jenkins'
-    static String src = "$jenkinsHome/src"
+    static String src = "$jenkinsHome/lsFusion/mycompany"
     static String tmp = "/tmp"
     static String download = "/var/www/download"
     static String rpm = "$jenkinsHome/rpm"
     static String apt = "$jenkinsHome/apt"
     static String ssl = "$jenkinsHome/ssl"
     static String signedDir = "$ssl/signed"
-    static String documentation = "$jenkinsHome/docusaurus"
+    static String documentation = "$jenkinsHome/docs"
     static String antlr4 = "/antlr"
     static String lsfLogics = "server/src/main/antlr3/lsfusion/server/language/LsfLogics.g"
 
-    static String githubRepo = "lsfusion/platform.git"
-    static String documentationRepo = "lsfusion/docusaurus.git"
-    static String docsRepo = "lsfusion/platform.git"
+    static String githubRepo = "lsfusion-solutions/mycompany.git"
+    static String documentationRepo = "lsfusion-solutions/mycompany-docs.git"
+    static String docsRepo = "lsfusion-solutions/mycompany.git"
 
     // for testing purposes
     static String githubIssuesRepo = null
