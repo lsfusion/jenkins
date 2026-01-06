fece2a63f3ed17722e56a754f2aaf1080adea180
pavel-miniutka <pavel.miniutka@gmail.com>
2026-01-06 11:20:56 +0300

    Documentation deploy

--- vars/deployDocumentation.groovy
+++ vars/deployDocumentation.groovy
@@ -1,27 +1,33 @@
 import static Paths.documentation
 
+def call() {
+    call("")
+}
+
 def call(String commitMessage) {
     dir(documentation) {
         git(
                 url: "git@github.com:$Paths.documentationRepo",
-                branch: 'master'
+                branch: 'main'
         )
         
         sh 'yarn install'
 
-        def list = readJSON text: new File("$documentation/update.config.json").text
-        list.each {
-            copyDocPages(it.id, it.branch)
-        }
+//        def list = readJSON text: new File("$documentation/update.config.json").text
+//        list.each {
+//            copyDocPages(it.id, it.branch)
+//        }
+        copyDocPages("next", "v6")
+
 
         sh "git add -A"
         sh "git diff-index --quiet HEAD || git commit -m 'Updated doc pages'"
-        sh "git push origin master"
+        sh "git push origin main"
 
-        sh "GIT_USER=nomojenkins USE_SSH=true DEPLOYMENT_BRANCH=master yarn deploy"
+        sh "GIT_USER=nomojenkins USE_SSH=true DEPLOYMENT_BRANCH=main yarn deploy"
 
-        if (commitMessage != null)
-            slack.message "Documentation has been successfully updated.\n```" + commitMessage + "```"
+//        if (commitMessage != null)
+//            slack.message "Documentation has been successfully updated.\n```" + commitMessage + "```"
     }
 }
 
@@ -29,6 +35,7 @@ def copyDocPages(id, branch) {
     def isNext = "next".equals(id)
     def enPath = isNext ? "$documentation/docs" : "$documentation/versioned_docs/version-$id"
     def ruPath = isNext ? "$documentation/i18n/ru/docusaurus-plugin-content-docs/current" : "$documentation/i18n/ru/docusaurus-plugin-content-docs/version-$id"
+    def plPath = isNext ? "$documentation/i18n/pl/docusaurus-plugin-content-docs/current" : "$documentation/i18n/pl/docusaurus-plugin-content-docs/version-$id"
 
     dir("$Paths.src") {
         git(
@@ -37,11 +44,15 @@ def copyDocPages(id, branch) {
         )
 
         dir("docs") {
-            sh "rm -rf $enPath"
-            sh "cp -fa en $enPath"
-
-            sh "rm -rf $ruPath"
-            sh "cp -fa ru $ruPath"
+//            sh "rm -rf $enPath"
+//            sh "cp -fa en $enPath"
+//
+//            sh "rm -rf $ruPath"
+//            sh "cp -fa ru $ruPath"
+            
+            sh "rsync -a en/ $enPath/"
+            sh "rsync -a ru/ $ruPath/"
+            sh "rsync -a pl/ $plPath/"
         }
     }
 }
