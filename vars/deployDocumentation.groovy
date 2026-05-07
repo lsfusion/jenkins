4c939fb1508506cd38749558294864d67365bee0
pavel-miniutka <pavel.miniutka@gmail.com>
2026-05-07 16:43:26 +0300

    MC docs: multiversion deploy

--- vars/deployDocumentation.groovy
+++ vars/deployDocumentation.groovy
@@ -13,21 +13,16 @@ def call(String commitMessage) {
         
         sh 'yarn install'
 
-//        def list = readJSON text: new File("$documentation/update.config.json").text
-//        list.each {
-//            copyDocPages(it.id, it.branch)
-//        }
-        copyDocPages("next", "v6")
-
+        def list = readJSON text: new File("$documentation/update.config.json").text
+        list.each {
+            copyDocPages(it.id, it.branch)
+        }
 
         sh "git add -A"
         sh "git diff-index --quiet HEAD || git commit -m 'Updated doc pages'"
         sh "git push origin main"
 
         sh "GIT_USER=nomojenkins USE_SSH=true DEPLOYMENT_BRANCH=main yarn deploy"
-
-//        if (commitMessage != null)
-//            slack.message "Documentation has been successfully updated.\n```" + commitMessage + "```"
     }
 }
 
@@ -44,12 +39,6 @@ def copyDocPages(id, branch) {
         )
 
         dir("src/main/resources/docs") {
-//            sh "rm -rf $enPath"
-//            sh "cp -fa en $enPath"
-//
-//            sh "rm -rf $ruPath"
-//            sh "cp -fa ru $ruPath"
-            
             sh "rsync -a en/ $enPath/"
             sh "rsync -a ru/ $ruPath/"
             sh "rsync -a pl/ $plPath/"
