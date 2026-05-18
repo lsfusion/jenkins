e59219f64cc3f88b70df7e6f373b8e7626557531
pavel-miniutka <pavel.miniutka@gmail.com>
2026-05-18 10:50:34 +0300

    overwriting versions

--- vars/releaseVersion.groovy
+++ vars/releaseVersion.groovy
@@ -2,11 +2,15 @@ def call(int branch, boolean releaseFinal) {
     boolean isBeta
     int majorVersion, minorVersion
     String tagVersion = "unknown"
+    String snapshotVersion = "unknown"
+    String nextVersion = "unknown"
 
     stage('Get branch version info') {
         (isBeta, minorVersion) = getBranchVersion(branch)
         majorVersion = branch
         tagVersion = majorVersion + '.' + minorVersion
+        snapshotVersion = "${majorVersion}.${minorVersion}-SNAPSHOT"
+        nextVersion = "${majorVersion}.${minorVersion + 1}-SNAPSHOT"
     }
 
     stage('Update') {
@@ -17,9 +21,25 @@ def call(int branch, boolean releaseFinal) {
         generateChangeLog(tagVersion)
         sh "mvn scm:checkin -Dmessage=\"Generated change log\""
     }
+    
+    stage('Prepare release version') {
+        sh """
+            sed -i '/<parent>/,/<\\/parent>/ s|<version>${snapshotVersion}</version>|<version>${tagVersion}</version>|' pom.xml
+            sed -i 's|<platform.assemble.version>${snapshotVersion}</platform.assemble.version>|<platform.assemble.version>${tagVersion}</platform.assemble.version>|' pom.xml
+        """
+        sh "mvn scm:checkin -Dmessage=\"Preparing for release: platform v${tagVersion}\""
+    }
 
     stage('Release branch') {
         sh "mvn -B release:clean release:prepare release:perform -P assemble -Dplatform.assemble.version=${tagVersion}"
+    }
+    
+    stage('Prepare next development version') {
+        sh """
+            sed -i '/<parent>/,/<\\/parent>/ s|<version>${tagVersion}</version>|<version>${nextVersion}</version>|' pom.xml
+            sed -i 's|<platform.assemble.version>${tagVersion}</platform.assemble.version>|<platform.assemble.version>${nextVersion}</platform.assemble.version>|' pom.xml
+        """
+        sh "mvn scm:checkin -Dmessage=\"Platform v${nextVersion}\""
     }
 
     stage('Update tag') {
